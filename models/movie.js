11ebd2a8982d094573cb63d754d9cde1002dac57
Steve Geluso <stevegeluso@gmail.com>
2018-05-31 09:24:10 -0700

    removing unused files

--- models/movie.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const MovieSchema = new mongoose.Schema({
-  title: String,
-  year: Number,
-});
-
-module.exports = mongoose.model('Movie', MovieSchema);
-
