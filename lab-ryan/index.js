f9f0885d17284c5fd3fbdf797356b0177977927a
rjtj2007 <teacherman1@gmail.com>
2018-06-02 15:55:17 -0700

    manually installing additional dependencies and building router

--- lab-ryan/index.js
+++ lab-ryan/index.js
@@ -6,10 +6,21 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({data: ['happyness', 1, 2, 3]});
-})
+const songRouter = require('./routes/songs.js');
+app.use('/songs', songRouter);
 
+app.get('/welcome', (req, res) => {
+    console.log('welcome to our song app');
+    res.send('welcome');
+});
+
+// app.get('/', (req, res) => {
+//     res.send({data: ['happyness', 1, 2, 3]});
+// })
+
+const Bundler = require('parcel-bundler');
+const bundler = new Bundler('./public/index.html');
+app.use(bundler.middleware());
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
